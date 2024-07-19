7bf514b929c7b58e6e28a4c98391f309e381ac80
june lee <melodysdreamj@gmail.com>
2024-07-20 06:56:47 +0900

    `

--- lib/util/_/build_app/function/before_material_app/insta_personal_page_lego/_.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-import '../../../../../../main.dart';
-
-// @ReadyBeforeMaterialApp()
-// Future<void> readyForInstaPersonalPageLego(BuildContext context) async {}
--- lib/util/_/build_app/widget/my_app/insta_personal_page_lego/_.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import '../../../../../../main.dart';
-
-// @CoverMyApp()
-// Widget Function() coverInstaPersonalPageLego(Widget Function() materialAppBuilder) {
-//   return () => materialAppBuilder();
-// }
